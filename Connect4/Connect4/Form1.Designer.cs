82ddfd1e1c14f937514f44fae7bbb2931369f2dc
HCHegarty <33227294+HCHegarty@users.noreply.github.com>
2019-02-08 12:48:47 +0000

    Add files via upload
    Added methods, reduced code by 300 lines, should help not lose marks for inefficiency

--- Connect4/Connect4/Form1.Designer.cs
+++ Connect4/Connect4/Form1.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(connect4Board));
-            System.Windows.Forms.Button btnBack;
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.reloadBoardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -40,7 +39,11 @@
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tmrChipFall = new System.Windows.Forms.Timer(this.components);
             this.textBox1 = new System.Windows.Forms.TextBox();
-            btnBack = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblPlayer1 = new System.Windows.Forms.Label();
+            this.lblPlayer2 = new System.Windows.Forms.Label();
+            this.lblPlayer1Score = new System.Windows.Forms.Label();
+            this.lblPlayer2Score = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -69,27 +72,27 @@
             // reloadBoardToolStripMenuItem
             // 
             this.reloadBoardToolStripMenuItem.Name = "reloadBoardToolStripMenuItem";
-            this.reloadBoardToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.reloadBoardToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
             this.reloadBoardToolStripMenuItem.Text = "Reload board";
             this.reloadBoardToolStripMenuItem.Click += new System.EventHandler(this.reloadBoardToolStripMenuItem_Click);
             // 
             // restartToolStripMenuItem
             // 
             this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
-            this.restartToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
             this.restartToolStripMenuItem.Text = "Restart";
             // 
             // helpToolStripMenuItem
             // 
             this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
-            this.helpToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
             this.helpToolStripMenuItem.Text = "Help";
             this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
             // 
             // muteToolStripMenuItem
             // 
             this.muteToolStripMenuItem.Name = "muteToolStripMenuItem";
-            this.muteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.muteToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
             this.muteToolStripMenuItem.Text = "Mute";
             // 
             // exitToolStripMenuItem
@@ -117,24 +120,65 @@
             this.textBox1.Text = resources.GetString("textBox1.Text");
             this.textBox1.Visible = false;
             // 
-            // btnBack
+            // button1
             // 
-            btnBack.Location = new System.Drawing.Point(12, 393);
-            btnBack.Name = "btnBack";
-            btnBack.Size = new System.Drawing.Size(378, 45);
-            btnBack.TabIndex = 2;
-            btnBack.Text = "Back";
-            btnBack.UseVisualStyleBackColor = true;
-            btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            this.button1.Location = new System.Drawing.Point(12, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(378, 45);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // lblPlayer1
+            // 
+            this.lblPlayer1.AutoSize = true;
+            this.lblPlayer1.Location = new System.Drawing.Point(12, 393);
+            this.lblPlayer1.Name = "lblPlayer1";
+            this.lblPlayer1.Size = new System.Drawing.Size(45, 13);
+            this.lblPlayer1.TabIndex = 3;
+            this.lblPlayer1.Text = "Player 1";
+            // 
+            // lblPlayer2
+            // 
+            this.lblPlayer2.AutoSize = true;
+            this.lblPlayer2.Location = new System.Drawing.Point(355, 393);
+            this.lblPlayer2.Name = "lblPlayer2";
+            this.lblPlayer2.Size = new System.Drawing.Size(45, 13);
+            this.lblPlayer2.TabIndex = 4;
+            this.lblPlayer2.Text = "Player 2";
+            // 
+            // lblPlayer1Score
+            // 
+            this.lblPlayer1Score.AutoSize = true;
+            this.lblPlayer1Score.Location = new System.Drawing.Point(12, 425);
+            this.lblPlayer1Score.Name = "lblPlayer1Score";
+            this.lblPlayer1Score.Size = new System.Drawing.Size(13, 13);
+            this.lblPlayer1Score.TabIndex = 5;
+            this.lblPlayer1Score.Text = "0";
+            // 
+            // lblPlayer2Score
+            // 
+            this.lblPlayer2Score.AutoSize = true;
+            this.lblPlayer2Score.Location = new System.Drawing.Point(355, 425);
+            this.lblPlayer2Score.Name = "lblPlayer2Score";
+            this.lblPlayer2Score.Size = new System.Drawing.Size(13, 13);
+            this.lblPlayer2Score.TabIndex = 6;
+            this.lblPlayer2Score.Text = "0";
             // 
             // connect4Board
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(402, 450);
-            this.Controls.Add(btnBack);
+            this.Controls.Add(this.lblPlayer2Score);
+            this.Controls.Add(this.lblPlayer1Score);
+            this.Controls.Add(this.lblPlayer2);
+            this.Controls.Add(this.lblPlayer1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "connect4Board";
@@ -158,6 +202,11 @@
         private System.Windows.Forms.ToolStripMenuItem muteToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblPlayer1;
+        private System.Windows.Forms.Label lblPlayer2;
+        private System.Windows.Forms.Label lblPlayer1Score;
+        private System.Windows.Forms.Label lblPlayer2Score;
     }
 }
 
